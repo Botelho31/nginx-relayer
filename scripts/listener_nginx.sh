0f2c72c8f7f0a2359d840f509896f94eedd81f35
Lucas Botelho <lucas.vbotelho83@gmail.com>
2022-02-16 17:36:52 -0300

    feat: now reloading on new config

--- scripts/listener_nginx.sh
+++ scripts/listener_nginx.sh
@@ -4,5 +4,5 @@ while true
 do
        touch  ./lastwatch
        sleep 5
-       find ./listener -cnewer ./lastwatch -exec bash -c 'echo "################################## Reloading nginx" && nginx -s stop && nginx -s start' - {} \; 
+       find /etc/nginx/conf.d -cnewer ./lastwatch -exec bash -c 'echo "################################## Reloading nginx" && nginx -s stop && nginx -s start' - {} \; 
 done
