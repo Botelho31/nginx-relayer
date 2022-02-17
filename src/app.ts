37254892a86a7684e106e1731d0c031f9be5002a
Lucas Botelho <lucas.vbotelho83@gmail.com>
2022-02-17 15:30:32 -0300

    feat: add check for certificates after execution

--- src/app.ts
+++ src/app.ts
@@ -2,22 +2,25 @@ import fs from 'fs'
 import path from 'path'
 import util from 'util'
 import { exec } from 'child_process'
-const execPromise = util.promisify(exec);
-
 import { ServerConfig } from './domain/model/server-config'
+import net from 'net'
+import cron from 'node-cron'
 import GreenlockHelper from './infra/greenlock-helper'
+
+const execPromise = util.promisify(exec)
+const server = net.createServer()
 
 const serverConfig = require('../relay-config.json') as ServerConfig
 const pathToConfig = path.join(__dirname, '../build/conf/default.conf')
 
-function createDir(relativePath: string) {
+function createDir (relativePath: string) {
   const dir = path.join(__dirname, relativePath)
-  if (!fs.existsSync(dir)){
-    fs.mkdirSync(dir);
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir)
   }
 }
 
-function addHttpServer(serverName: String, relay: String, certificate: boolean, httpsPass: boolean) : String {
+function addHttpServer (serverName: String, relay: String, certificate: boolean, httpsPass: boolean) : String {
   let httpText = fs.readFileSync(path.join(__dirname, 'nginx-conf/http.txt'), 'utf8')
   httpText += `\nserver_name ${serverName};\n`
   if (certificate) {
@@ -28,7 +31,7 @@ function addHttpServer(serverName: String, relay: String, certificate: boolean, 
     const httpsPass = fs.readFileSync(path.join(__dirname, 'nginx-conf/https-pass.txt'), 'utf8')
     httpText += httpsPass
   } else {
-    httpText += 
+    httpText +=
       ` location / {
           proxy_pass http://${relay.replace('localhost', 'host.docker.internal')};
           proxy_buffering on;
@@ -40,9 +43,9 @@ function addHttpServer(serverName: String, relay: String, certificate: boolean, 
   return httpText
 }
 
-function addHttpsServer(serverName: String, relay: String) : String {
+function addHttpsServer (serverName: String, relay: String) : String {
   let httpsText = fs.readFileSync(path.join(__dirname, 'nginx-conf/https.txt'), 'utf8')
-  httpsText += 
+  httpsText +=
       ` server_name ${serverName};
         ssl_dhparam /etc/ssl/certs/dhparam-2048.pem;
         ssl_certificate /etc/letsencrypt/live/${serverName}/fullchain.pem;
@@ -57,15 +60,53 @@ function addHttpsServer(serverName: String, relay: String) : String {
   return httpsText
 }
 
-async function main() {
+async function checkForPortUsage (port: number) : Promise<Boolean> {
+  return new Promise((resolve) => {
+    server.once('error', function (err: any) {
+      if (err.code === 'EADDRINUSE') {
+        // port is currently in use
+        resolve(true)
+      }
+    })
+
+    server.once('listening', function () {
+      // close the server if listening doesn't fail
+      server.close()
+      resolve(false)
+    })
+
+    server.listen(port)
+  })
+}
+
+const checkForCertificates = cron.schedule('0 * * * * *', async () => {
+  console.log('Checking for certificates')
+  const relays = serverConfig.relays
+  const greenlock = new GreenlockHelper(serverConfig.contactEmail)
+  for (let i = 0; i < relays.length; i += 1) {
+    if (relays[i].https && fs.existsSync(path.join(`..build/certificates/${relays[i].serverName}/temporary`))) {
+      const httpsInUse = await checkForPortUsage(443)
+      const httpInUse = await checkForPortUsage(80)
+      if (httpInUse && httpsInUse) {
+        const certificate = await greenlock.getCertificate(relays[i].serverName)
+        fs.writeFileSync(path.join(__dirname, `../build/certificates/${relays[i].serverName}/fullchain.pem`), certificate.fullchain)
+        fs.writeFileSync(path.join(__dirname, `../build/certificates/${relays[i].serverName}/privkey.pem`), certificate.privkey)
+        fs.unlinkSync(path.join(__dirname, `../build/certificates/${relays[i].serverName}/temporary`))
+      }
+    }
+  }
+}, {
+  scheduled: false
+})
+
+async function main () {
   createDir('../build')
   createDir('../build/conf')
   fs.openSync(pathToConfig, 'w')
-  const greenlock =  new GreenlockHelper(serverConfig.contactEmail)
-  let nginxConf = ""
+  let nginxConf = ''
   const relays = serverConfig.relays
-  for(let i = 0;i < relays.length; i+= 1) {
-    nginxConf += addHttpServer(relays[i].serverName , relays[i].relay, relays[i].https, relays[i].forceHttps)
+  for (let i = 0; i < relays.length; i += 1) {
+    nginxConf += addHttpServer(relays[i].serverName, relays[i].relay, relays[i].https, relays[i].forceHttps)
     if (relays[i].https) {
       nginxConf += addHttpsServer(relays[i].serverName, relays[i].relay)
       createDir('../build/dhparam/')
@@ -74,14 +115,16 @@ async function main() {
         createDir(`../build/certificates/${relays[i].serverName}`)
         const certificatePath = path.join(`build/certificates/${relays[i].serverName}`)
         console.log(`Creating Dummy Certificates for ${relays[i].serverName}`)
-        await execPromise(`openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ${certificatePath}/privkey.pem -out ${certificatePath}/fullchain.pem -subj "/C=${serverConfig.address.country}/ST=${serverConfig.address.city}/L=${serverConfig.address.neighborhood}/O=${serverConfig.project} /OU=IT Department/CN=${relays[i].serverName}"`);
+        await execPromise(`openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ${certificatePath}/privkey.pem -out ${certificatePath}/fullchain.pem -subj "/C=${serverConfig.address.country}/ST=${serverConfig.address.city}/L=${serverConfig.address.neighborhood}/O=${serverConfig.project} /OU=IT Department/CN=${relays[i].serverName}"`)
+        fs.openSync(path.join(`..build/certificates/${relays[i].serverName}/temporary`), 'w')
       }
       const dhparamPath = path.join(__dirname, '../build/dhparam/dhparam-2048.pem')
       if (!fs.existsSync(dhparamPath)) {
-        fs.writeFileSync(dhparamPath, require("dhparam")())
+        fs.writeFileSync(dhparamPath, require('dhparam')())
       }
     }
   }
+  checkForCertificates.start()
   fs.appendFileSync(pathToConfig, nginxConf)
 }
 
--- src/infra/greenlock-helper.ts
+++ src/infra/greenlock-helper.ts
@@ -1,48 +1,46 @@
-'use strict';
-import { Certificate } from 'crypto';
-import path from 'path';
-const pkg = require('../../package.json');
-const Greenlock = require('greenlock');
+'use strict'
+const pkg = require('../../package.json')
+const Greenlock = require('greenlock')
 
 export default class GreenlockHelper {
   private greenlock;
 
   constructor (contactEmail: String) {
     this.greenlock = Greenlock.create({
-        packageRoot: __dirname,
-        configDir: "../../greenlock.d/",
-        packageAgent: pkg.name + '/' + pkg.version,
-        maintainerEmail: contactEmail,
-        staging: true,
-        notify: function(event: any, details: any) {
-            if ('error' === event) {
-                // `details` is an error object in this case
-                console.error(details);
-            }
+      packageRoot: __dirname,
+      configDir: '../../greenlock.d/',
+      packageAgent: pkg.name + '/' + pkg.version,
+      maintainerEmail: contactEmail,
+      staging: true,
+      notify: function (event: any, details: any) {
+        if (event === 'error') {
+          // `details` is an error object in this case
+          console.error(details)
         }
-    });
+      }
+    })
   }
 
   async getCertificate (serverName: String) : Promise<any> {
     await this.greenlock.add({
-        subject: serverName,
-        altnames: [serverName]
+      subject: serverName,
+      altnames: [serverName]
     })
     const result = await this.greenlock.get({ servername: serverName })
     if (!result) {
-        // certificate is not on the approved list
-        return null;
+      // certificate is not on the approved list
+      return null
     }
     if (result.pems && result.pems.privkey && result.pems.cert && result.pems.chain) {
-      console.info('Success');
+      console.info('Success')
     }
 
-    var fullchain = result.pems.cert + '\n' + result.pems.chain + '\n';
-    var privkey = result.pems.privkey;
+    const fullchain = result.pems.cert + '\n' + result.pems.chain + '\n'
+    const privkey = result.pems.privkey
 
     return {
-        fullchain,
-        privkey
-    };
+      fullchain,
+      privkey
+    }
   }
 }
