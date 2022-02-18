1832abc9188b3627318bce1ef2d74b4d973f32a1
Lucas Botelho <lucas.vbotelho83@gmail.com>
2022-02-18 15:07:00 -0300

    feat: change docker container name

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env sh
 
-export EMAIL=lucas.vbotelho83@gmail.com;
-export PROJECT=botelho;
-export DNS=botelho.club;
 docker-compose build;
 docker-compose up -d --force-recreate
--- scripts/update_run.sh
+++ scripts/update_run.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env sh
 
 git pull;
-export EMAIL=lucas.vbotelho83@gmail.com;
-export PROJECT=botelho;
-export DNS=botelho.club;
 docker-compose build;
 docker-compose up -d --force-recreate;
