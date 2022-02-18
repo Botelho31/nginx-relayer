5633c57c598a4d44183637c1cd0e82deee8a5993
Lucas Botelho <lucas.vbotelho83@gmail.com>
2022-02-18 15:55:19 -0300

    feat: now n copying entire source

--- scripts/run_nginx.sh
+++ scripts/run_nginx.sh
@@ -2,5 +2,4 @@ echo "################################## Create File Listener"
 ./src/scripts/listener_nginx.sh &
 
 echo "################################## Run nginx"
-mkdir /challenge
 nginx -g "daemon off;"
