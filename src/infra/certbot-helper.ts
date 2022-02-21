297a6d1bc2d342658ff7e0159d77ea0cf5b8e64b
Lucas Botelho <lucas.vbotelho83@gmail.com>
2022-02-21 18:15:57 -0300

    feat: add challenge path

--- src/infra/certbot-helper.ts
+++ src/infra/certbot-helper.ts
@@ -1,6 +1,7 @@
 import { exec } from 'child_process'
 import util from 'util'
 import fs from 'fs'
+import path from 'path'
 import { Certificate } from '../domain/model/certificate'
 
 const execPromise = util.promisify(exec)
@@ -12,7 +13,8 @@ export default class CertbotHelper {
   }
 
   async getCertificate (serverName: String) : Promise<Certificate> {
-    await execPromise(`certbot certonly --webroot --webroot-path=/var/www/html --email ${this.contactEmail} --agree-tos --noninteractive --no-eff-email -d ${serverName}`)
+    const challengePath = path.join(__dirname, '../../build/challenge')
+    await execPromise(`certbot certonly --webroot --webroot-path=${challengePath} --email ${this.contactEmail} --agree-tos --noninteractive --no-eff-email -d ${serverName}`)
     const fullchain = fs.readFileSync(`/etc/letsencrypt/live/${serverName}/fullchain.pem`, 'utf8')
     const privkey = fs.readFileSync(`/etc/letsencrypt/live/${serverName}/privkey.pem`, 'utf8')
     return {
