e25b740f7d641aca95c46a3da194a123e2b30868
Lucas Botelho <lucas.vbotelho83@gmail.com>
2022-02-22 19:48:55 -0300

    feat: fix to naming error

--- scripts/clean_docker.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-docker rm -f $(docker ps -a -q);
-docker volume rm $(docker volume ls -q);
-docker rmi -f $(docker images -a -q);
--- scripts/install_certbot.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-
-sudo snap install core; 
-sudo snap refresh core;
-sudo snap install --classic certbot;
-ln -s /snap/bin/certbot /usr/bin/certbot;
