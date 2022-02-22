a54e721106f4bb4077d08b1757e03c91028c37e0
Lucas Botelho <lucas.vbotelho83@gmail.com>
2022-02-22 17:28:15 -0300

    feat: add free write peermissions

--- scripts/run_nginx.sh
+++ scripts/run_nginx.sh
@@ -2,4 +2,5 @@ echo "################################## Create File Listener"
 ./scripts/listener_nginx.sh &
 
 echo "################################## Run nginx"
+chmod -R 755 /etc/nginx/conf.d/* 
 nginx -g "daemon off;"
